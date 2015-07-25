bef79db609984f05db1a49c294986e31b20c2f95
JAM <jacjam@gmail.com>
2015-07-24 20:11:52 -0600

    Added middleware that handles internal errors. Added logger module. Added logger configuration

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-'use strict';
-
 var unprocessableEntityMW = require('unprocessable-entity-mw');
 var notFoundMW            = require('not-found-mw');
+var internalErrorMW       = require('internal-error-mw');
 var SwaggerExpress        = require('swagger-express-mw');
 var app                   = require('express')();
+var logger                = require('logger-initializer');
+
 module.exports = app; // for testing
 
 var config = {
@@ -13,14 +14,17 @@ var config = {
 SwaggerExpress.create(config, function(err, swaggerExpress) {
   if (err) { throw err; }
 
+  logger = logger(swaggerExpress.config.logger.logLevel);
+
   // install middleware
   swaggerExpress.register(app);
 
   app.use(notFoundMW);
   app.use(unprocessableEntityMW);
+  app.use(internalErrorMW);
 
   var port = process.env.PORT || 10010;
   app.listen(port);
 
-  console.log('Email templates microservice started on port ' + port);
+  logger.info('Email templates microservice started on port ' + port);
 });
